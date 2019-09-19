dffb11e8e5e4117abd6c7b86ffeb69b75c7391c3
lopphong12 <54985461+lopphong12@users.noreply.github.com>
2019-09-19 15:08:06 +0700

    Update redirectcopy_bconsgarden.js

--- redirectcopy_bconsgarden.js
+++ redirectcopy_bconsgarden.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>Muon copy thi vao: <a href='http://diaocbcons.com/du-an-bcons-garden/'>http://diaocbcons.com/du-an-bcons-garden/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Muon copy thi vao: <a href='http://diaocbcons.com/du-an-bcons-garden/'>http://diaocbcons.com/du-an-bcons-garden"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
